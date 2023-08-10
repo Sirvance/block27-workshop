7a240eaa6fe7aa305ddd1a019b6b9df3afacc1ac
vance <vancedowdell2110@yahoo.com>
2023-08-10 16:53:52 -0700

    new

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,46 +1,115 @@
 import React, { useState } from 'react';
 
 
-export default function SignUpForm() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
-    
-    
-    
-    
-    async function handleSubmit(event) {
-        event.preventDefault();
+export default function SignUpForm({ setToken }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    try {
+      const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
-        try {
-            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ username, password }), // Send username and password in the request body
-            });
-      
-            const result = await response.json();
-            if (result.token) {
-                setToken(result.token)
-            }  
-           
-        } catch (error) {
-            setError(error.message);
-          }
-        }
-      return (
-        <>
+      const result = await response.json();
+      setToken(result.token);
+    } catch (error) {
+      console.error('Error signing up:', error.message);
+    }
+  }
+
+  return (
+    <div>
       <h2>Sign Up</h2>
-      {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
-        {/* ... */}
+        {/* Form fields for email and password */}
+        <button type="submit">Sign Up</button>
       </form>
-    </>
+    </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function SignUpForm() {
+//     const [username, setUsername] = useState("");
+//     const [password, setPassword] = useState("");
+//     const [error, setError] = useState(null);
+    
+    
+    
+    
+//     async function handleSubmit(event) {
+//         event.preventDefault();
+
+
+//         try {
+//             const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+//               method: "POST",
+//               headers: {
+//                 "Content-Type": "application/json",
+//               },
+//               body: JSON.stringify({ username, password }), // Send username and password in the request body
+//             });
+      
+//             const result = await response.json();
+//             if (result.token) {
+//                 setToken(result.token)
+//             }  
+           
+//         } catch (error) {
+//             setError(error.message);
+//           }
+//         }
+//       return (
+//         <>
+//       <h2>Sign Up</h2>
+//       {error && <p>{error}</p>}
+//       <form onSubmit={handleSubmit}>
+//         {/* ... */}
+//       </form>
+//     </>
+//   );
+// }
     //     <div>
     //       <h2>Sign Up!</h2>
     //       <form onSubmit={handleSubmit}>
